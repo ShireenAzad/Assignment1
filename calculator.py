abc63d99ff85acd8f99bbcc9f11739b35d26a96d
shireen <syed.shireen@everest.engineering>
2021-08-09 20:53:26 +0530

    modifications and conflicts resolved

--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 def add(l):
 	sum=0
 	for i in l:
 		sum+=i
 	return sum
-||||||| 8e832e3
-=======
+#||||||| 8e832e3
+#=======
 def subtract(l):
 	diff=l[0]
-	length=len(l1)
+	length=len(l)
 	for i in range(1,length):
 		diff-=l[i]
 	return diff
 
->>>>>>> feature/subtraction
+#>>>>>>> feature/subtraction
